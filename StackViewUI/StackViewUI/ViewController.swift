ec943c40d1ea9a05085a3e316fe08e8848236cc5
wackyjoker <kevin.young8931@gmail.com>
2017-09-02 17:32:42 +0200

    self made Converse UI

---
+++ StackViewUI/StackViewUI/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  StackViewUI
+//
+//  Created by kevin on 02/09/2017.
+//  Copyright © 2017 Kevin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var Button: UIStackView!
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        let orientation = UIApplication.shared.statusBarOrientation
+    
+        if orientation.isPortrait {
+            self.Button.axis = .horizontal
+        }
+        else {
+            self.Button.axis = .vertical
+        
+        }
+    
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
